c274bead96dbb7933a5b983742f239fceedb4dfa
MahboubAnas05-20 <mahboub.anas05@gmail.com>
2026-02-03 17:30:59 +0100

    i created the controller with it methods

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,41 +4,52 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-                                          
+use App\Models\Product;
+
 class ProductController extends Controller
 {
-    // public $products = [
-    //     [
-    //     'id' => 1,
-    //     'title' => 'HP16D0195NF',
-    //     'price' => 'Intel i5 12th Gen',
-    //     'image' => 'HP16D0195NF.jpg',
-    //     'description' => 'Laptop polyvalent conçu pour la bureautique et
-    //    multimédia.'
-    //     ],
-    //     [
-    //     'id' => 2,
-    //     'title' => 'HP14424U3EA',
-    //     'price' => 'Intel i3 11th Gen',
-    //     'image' => 'HP14424U3EA.jpg',
-    //     'description' => 'Portable léger adapté aux tâches quotidiennes.'
-    //     ],
-    //     [
-    //     'id' => 3,
-    //     'title' => 'HUA6901443442959',
-    //     'price' => 'Ryzen 5 4600H',
-    //     'image' => 'HUA6901443442959.jpg',
-    //     'description' => 'Bonne performance pour bureautique + montage
-    //    léger.'
-    //     ],
-    //     [
-    //     'id' => 4,
-    //     'title' => 'NXATHEF002',
-    //     'price' => 'Intel i7 10th Gen',
-    //     'image' => 'NXATHEF002.jpg',
-    //     'description' => 'Portable professionnel avec écran Full HD.'
-    //     ],
-    //    ];
+    public $products = [
+        [
+        'id' => 1,
+        'title' => 'HP16D0195NF',
+        'price' => 'Intel i5 12th Gen',
+        'image' => 'HP16D0195NF.jpg',
+        'description' => 'Laptop polyvalent conçu pour la bureautique et
+       multimédia.'
+        ],
+        [
+        'id' => 2,
+        'title' => 'HP14424U3EA',
+        'price' => 'Intel i3 11th Gen',
+        'image' => 'HP14424U3EA.jpg',
+        'description' => 'Portable léger adapté aux tâches quotidiennes.'
+        ],
+        [
+        'id' => 3,
+        'title' => 'HUA6901443442959',
+        'price' => 'Ryzen 5 4600H',
+        'image' => 'HUA6901443442959.jpg',
+        'description' => 'Bonne performance pour bureautique + montage
+       léger.'
+        ],
+        [
+        'id' => 4,
+        'title' => 'NXATHEF002',
+        'price' => 'Intel i7 10th Gen',
+        'image' => 'NXATHEF002.jpg',
+        'description' => 'Portable professionnel avec écran Full HD.'
+        ],
+       ];
 
+    public function index(){
+        return view("products.index", ['products'=>$this->products]);
+    }
+    public function show($id){
+        $product = $this->products[$id];
+        if(!isset($product)){
+            abort(404, 'produit intouvable');
+        }
+        return view("products.show", ['product'=>$product]);
+    }
     
 }
